cb88bba39cceea430b4ba7f003e4c8a87a702368
maatheryousuf <130503047+maatheryousuf@users.noreply.github.com>
2023-05-09 16:43:45 +0400

    Update First problem of the day.py
    by: Maather Yousuf, Fatema Alsaaidi, Maya Alhatmi, and Asma Almoqbali

--- First problem of the day.py
+++ First problem of the day.py
@@ -1,7 +1,7 @@
 n1= int(input("Enter number 1="))
 n2= int(input("Enter number 2="))
 n3= int(input("Enter number 3="))
-        
+      
 numbers=[n1, n2, n3]
 
 if numbers[0]== numbers[1]== numbers[2]:
