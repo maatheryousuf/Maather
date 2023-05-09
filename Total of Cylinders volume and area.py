1c4f3603e52a1b080b617417acfbc1b245c4f28f
maatheryousuf <130503047+maatheryousuf@users.noreply.github.com>
2023-05-09 13:29:32 +0400

    Add files via upload

---
+++ Total of Cylinders volume and area.py
@@ -0,0 +1,17 @@
+h= 4
+r= 6
+PI= 22/7
+
+Cylinder_volume= PI*r**2*h
+area= (2*PI*r*h) + (2*PI*r**2)
+
+print(Cylinder_volume)
+print(area)
+
+""" print two difgit after the deimal point"""
+
+Cylinder_volume= int (Cylinder_volume*100)/100.0
+print (Cylinder_volume)
+
+area= int (area*100)/100.0
+print (area)
