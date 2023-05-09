19ffa77762af44073fbfd65e8c11a9c251b83462
maatheryousuf <130503047+maatheryousuf@users.noreply.github.com>
2023-05-09 14:06:58 +0400

    Add files via upload

---
+++ Total cost of purchase 10 tons of iron with 5% vat.py
@@ -0,0 +1,14 @@
+""" Total cost of purchase 10 tons of iron with 5% vat """
+"""the value of one ton is one rial"""
+
+number_tons = 10
+value = 1
+
+Total_cost = number_tons * value
+print (Total_cost, "OR")
+
+vat = Total_cost * 0.05
+print (vat, "OR")
+
+Total_cost = Total_cost + vat
+print(Total_cost, "OR")
