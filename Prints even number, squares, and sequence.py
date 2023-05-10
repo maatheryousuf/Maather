141415a89f07c1b635955fa2aa3cf78c699ca532
maatheryousuf <130503047+maatheryousuf@users.noreply.github.com>
2023-05-10 20:23:46 +0400

    Add files via upload

---
+++ Prints even number, squares, and sequence.py
@@ -0,0 +1,21 @@
+""" prints the first 10 even numbers using while loop """
+i = 0
+
+while (i<=10):
+    if i%2==0 :
+       print(i)
+    i += 1
+
+""" prints the first 10 numbers with their squares usig while loop"""
+i=1
+while(i<=10):
+  print(i, i**2)
+  i += 1
+
+""" prints the following sequence (105, 98, 91, ....., 7)  """
+
+i= 105
+
+while (i>= 7):
+    print (i)
+    i -= 7
