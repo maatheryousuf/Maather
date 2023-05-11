c490d0a89fa836c945d46e276962bb10ea44ec55
maatheryousuf <130503047+maatheryousuf@users.noreply.github.com>
2023-05-11 10:14:34 +0400

    Add files via upload

---
+++ convert from while loop to for loop.py
@@ -0,0 +1,13 @@
+""" Open ' The average of the grades ' file to see the while loop """
+
+total = 0
+number = int(input("The number of element:"))            
+for j in range(number):
+    num= int(input("Enter the number:"))
+    total += num
+      
+print(total/number)
+
+   
+
+    
