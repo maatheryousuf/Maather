8cd1557973a7f277d84ca678114843267013d161
maatheryousuf <130503047+maatheryousuf@users.noreply.github.com>
2023-05-15 09:53:13 +0400

    sumDigits.py

--- sumDigits.py
+++ sumDigits.py
@@ -1,3 +1,5 @@
+""" string, int """
+
 dig = input("Enter a digit :")
 
 def summ(d):
@@ -9,5 +11,21 @@ def summ(d):
     
 result = summ(dig)
 print (result)
+
+""" int, string, int """
+
+dig = int(input("Enter a digit :"))
+
+def summ(d):
+  t = str(dig)
+  a = 0
+  for i in t:
+    i = int(i)
+    a = a + i
+  return a
+    
+result = summ(dig)
+print (result)
+
 
 
