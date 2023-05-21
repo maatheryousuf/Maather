f8cea5475d10518a0ef06ddad099e113f742373e
maatheryousuf <130503047+maatheryousuf@users.noreply.github.com>
2023-05-21 22:05:07 +0400

    Update problem21-5.py

--- problem21-5.py
+++ problem21-5.py
@@ -1,4 +1,4 @@
-
+"""Print the number with the highest number of repeating elements"""
 numbers = [1,8,7,4,1,2,2,2]
 
 counter = 0
@@ -12,4 +12,24 @@ for i in numbers:
 
 print("The original list ",numbers)      
 print("The number with the highest number of repeating elements is ",highest_number)
+
+
+"""Print all numbers that have duplicates numbers"""
+
+numbers = [1,8,7,4,1,2,2,2]
+listt = []
+
+
+for i in numbers:
+    s = numbers.count(i)
+    if s >1 :
+        if listt.count(i) == 0:
+            listt.append(i)
+        
+print("The total number of elements that has duplicate numbers",i)
+print("The original list ",numbers)
+print("The new list for numbers that have duplicates",listt)
+
+
+
 
